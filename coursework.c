f413f38a8f31f9041051ded060eb9adecda46880
isabellewalford <issywalf@gmail.com>
2024-02-01 17:27:05 +0000

    q1

--- coursework.c
+++ coursework.c
@@ -1,3 +1,20 @@
+#include <stdio.h>
 
+int printBytes (void *p, int);
+
+// main 
+int main(void) {
+    
+
+    return 0;
+}
 
 // Question 1
+int printBytes (void *p, int bytes)
+{
+    for (i=0; i<b; i++)
+    {
+        printf("00%d : %d", i+1, *(int *)p);
+        p = ((char*)p) + 1;
+    }
+}

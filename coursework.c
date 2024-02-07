e9f01b1c6ab3d04ed74e27bef0f300bca1ccb15f
isabellewalford <issywalf@gmail.com>
2024-02-07 19:20:36 +0000

    started q2

--- coursework.c
+++ coursework.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int printBytes (void *p, int);
+int printBytes (void *, int);
+int reverseText(char [], char []);
 
 
-int main(void) {
+int main(int argc, char **argv) {
 
     char arr[4] = {'A','B','C','D'};
     printBytes(&arr, 4);
@@ -22,4 +24,45 @@ int printBytes (void *p, int bytes)
         printf("00%d : %d\n",i+1, *(unsigned char *)p);
         p= ((char *)p)+ 1;
     }
+    return 0;
+}
+
+/*Question 2*/
+int reverseText(char fileIn[], char fileOut[])
+{
+    FILE *fpIn;
+    FILE *fpOut;
+    char ch;
+    int count;
+
+    if ((fpIn = fopen(fileIn, "r")) == NULL)
+    {
+        printf("Unable to read file: %d: %s\n", errno, strerror(errno));
+        exit(1);
+    }
+
+    if ((fpOut = fopen(fileOut, "r")) == NULL)
+    {
+        fclose(fpOut);
+        fpOut = fopen(fileOut, "w");
+    }
+    else 
+    {
+        fclose(fpOut);
+        fpOut = fopen(fileOut, "a");
+    }
+    
+    while ( !feof(fp) )
+   {
+      ch = fgetc(fp);
+        if( ch == EOF || ch == '\n' )
+        {
+            break;
+        }
+        ++count;
+   }
+
+    fclose(fpIn);
+    fclose(fpOut);
+    return 0;
 }
